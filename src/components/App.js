a10cc4d3c578616e5f91874479bb7811e287a52f
thneves <tsneves11@gmail.com>
2021-06-21 15:14:25 -0300

    Inset Calculate func into APP component

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,13 @@ import '../index.css';
 import Display from './Display';
 import Panel from './Panel';
 import '../styles/App.css';
+import Calculate from '../logic/calculate';
 
 function App() {
   return (
     <>
       <div className="calculator">
-        <Display result="10" />
+        <Display result={Calculate(null, null)} />
         <Panel />
       </div>
     </>
