c6fce0cf98fc88279dc9b3d3762db0d186c11b8a
thneves <tsneves11@gmail.com>
2021-06-21 18:22:45 -0300

    Change App to a class component, add initial states

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,27 @@ import Panel from './Panel';
 import '../styles/App.css';
 import Calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <>
-      <div className="calculator">
-        <Display result={Calculate(null, null)} />
-        <Panel />
-      </div>
-    </>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <div className="calculator">
+          <Display result={Calculate(null, null)} />
+          <Panel />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
