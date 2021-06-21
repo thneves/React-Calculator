43cec3e74980739fb73c393d0a9c9ec85251a579
thneves <tsneves11@gmail.com>
2021-06-21 15:16:06 -0300

    Fix linter

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div className="calculator">
-        <Display result={Calculate(null, null)} />
+        <Display result={ Calculate(null,null) } />
         <Panel />
       </div>
     </>
