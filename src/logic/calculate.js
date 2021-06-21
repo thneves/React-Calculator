bcb8b3e7b421b5b775b7ca3b4b2bcaad09e6faee
thneves <tsneves11@gmail.com>
2021-06-21 01:37:13 -0300

    Create logic folder and files

---
+++ src/logic/calculate.js
@@ -0,0 +1,8 @@
+
+function Calculate(calc, button) {
+  let { total, next, operation} = calc;
+
+  
+}
+
+export default Calculate;
---
+++ src/logic/operate.js
@@ -0,0 +1,7 @@
+import Big from 'big.js';
+
+function Operate(numOne, numTwo, operation) {
+
+}
+
+export default Operate;
