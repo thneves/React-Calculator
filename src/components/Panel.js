f0fd0e11a005d7a15035d556f848eac98fb3c78a
thneves <tsneves11@gmail.com>
2021-06-23 15:18:41 -0300

    Add clickHandler to Panel component

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,37 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Button from './Button';
 import '../styles/Panel.css';
 
-function Panel() {
+function Panel({ clickHandler }) {
   return (
     <>
       <div className="panel">
         <div className="panel-row">
-          <Button name="AC" />
-          <Button name="+/-" />
-          <Button name="%" />
-          <Button name="/" />
+          <Button name="AC" clickHandler={() => clickHandler('AC')} />
+          <Button name="+/-" clickHandler={() => clickHandler('+/-')} />
+          <Button name="%" clickHandler={() => clickHandler('%')} />
+          <Button name="/" clickHandler={() => clickHandler('/')} />
         </div>
         <div className="panel-row">
-          <Button name="7" />
-          <Button name="8" />
-          <Button name="9" />
-          <Button name="X" />
+          <Button name="7" clickHandler={() => clickHandler('7')} />
+          <Button name="8" clickHandler={() => clickHandler('8')} />
+          <Button name="9" clickHandler={() => clickHandler('9')} />
+          <Button name="X" clickHandler={() => clickHandler('X')} />
         </div>
         <div className="panel-row">
-          <Button name="5" />
-          <Button name="6" />
-          <Button name="7" />
-          <Button name="+" />
+          <Button name="5" clickHandler={() => clickHandler('5')} />
+          <Button name="6" clickHandler={() => clickHandler('6')} />
+          <Button name="7" clickHandler={() => clickHandler('7')} />
+          <Button name="+" clickHandler={() => clickHandler('+')} />
         </div>
         <div className="panel-row">
-          <Button name="0" />
-          <Button name="." />
-          <Button name="=" />
+          <Button name="0" clickHandler={() => clickHandler('0')} />
+          <Button name="." clickHandler={() => clickHandler('.')} />
+          <Button name="=" clickHandler={() => clickHandler('=')}/>
         </div>
       </div>
     </>
   );
 }
+
+Panel.propTypes = {
+  clickHandler: PropTypes.func.isRequired,
+};
 
 export default Panel;
